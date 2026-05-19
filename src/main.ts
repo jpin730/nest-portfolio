dec86d07905f7ee66d29c596c7df7c3c5dbc6a66
Jaime Pineda <jpin730@pm.me>
2026-05-18 18:57:15 -0600

    feat: :loud_sound: add morgan http logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core'
+import morgan from 'morgan'
 
 import { AppModule } from './app.module'
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
+
+  app.use(morgan('tiny'))
+
   await app.listen(process.env.PORT ?? 3000)
 }
 void bootstrap()
