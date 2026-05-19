01d0c32780b7ffa990867c8b9553e291a9857a1c
Jaime Pineda <jpin730@pm.me>
2026-05-18 18:17:10 -0600

    chore: :wrench: add eslint, prettier, husky and lint-staged configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,21 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslintJs from '@eslint/js'
+import prettier from 'eslint-plugin-prettier/recommended'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+export default defineConfig([
   {
     ignores: ['eslint.config.mjs'],
   },
-  eslint.configs.recommended,
+  eslintJs.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+  prettier,
   {
     languageOptions: {
       globals: {
         ...globals.node,
-        ...globals.jest,
       },
       sourceType: 'commonjs',
       parserOptions: {
@@ -26,10 +26,12 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
+      'no-console': ['error', { allow: ['info'] }],
+      'no-debugger': 'error',
     },
   },
-);
+])
