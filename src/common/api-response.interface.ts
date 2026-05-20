a164695f2c9a46341003aafbc39cbda7686c9860
Jaime Pineda <jpin730@pm.me>
2026-05-20 17:11:41 -0600

    refactor: :recycle: enhance global exception filter to support multiple message formats in ApiResponse

--- src/common/api-response.interface.ts
+++ src/common/api-response.interface.ts
@@ -1,4 +1,4 @@
 export interface ApiResponse {
-  message?: string
+  message?: string | string[]
   data?: unknown
 }
--- src/common/global-exception.filter.ts
+++ src/common/global-exception.filter.ts
@@ -5,6 +5,7 @@ import {
   ExceptionFilter,
   HttpException,
   HttpStatus,
+  Logger,
 } from '@nestjs/common'
 import { APP_FILTER, HttpAdapterHost } from '@nestjs/core'
 
@@ -12,6 +13,8 @@ import { ApiResponse } from './api-response.interface'
 
 @Catch()
 class GlobalExceptionFilter implements ExceptionFilter<unknown> {
+  private readonly logger = new Logger(GlobalExceptionFilter.name)
+
   constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
 
   catch(exception: unknown, host: ArgumentsHost): void {
@@ -19,19 +22,56 @@ class GlobalExceptionFilter implements ExceptionFilter<unknown> {
 
     const ctx = host.switchToHttp()
 
-    let statusCode = HttpStatus.INTERNAL_SERVER_ERROR
-    let message = HttpStatus[statusCode]
-
-    if (exception instanceof HttpException) {
-      statusCode = exception.getStatus()
-    }
-    if (exception instanceof Error && exception.message) {
-      message = exception.message
-    }
-
+    const statusCode = this.getStatusCode(exception)
+    const message = this.getMessage(exception)
     const body: ApiResponse = { message }
 
+    this.logger.error(message.toString())
+
     httpAdapter.reply(ctx.getResponse(), body, statusCode)
+  }
+
+  private getStatusCode(exception: unknown): number {
+    if (exception instanceof HttpException) {
+      return exception.getStatus()
+    }
+    return HttpStatus.INTERNAL_SERVER_ERROR
+  }
+
+  private getMessage(exception: unknown): string | string[] {
+    const isInstanceOfError = exception instanceof Error
+    if (!isInstanceOfError) {
+      return HttpStatus[HttpStatus.INTERNAL_SERVER_ERROR]
+    }
+
+    if (exception instanceof HttpException) {
+      const response = exception.getResponse()
+      const message = this.getMessageFromResponse(response)
+      if (message) {
+        return message
+      }
+    }
+
+    return exception.message
+  }
+
+  private getMessageFromResponse(response: unknown): string | string[] | null {
+    const isObjectWithMessage =
+      typeof response === 'object' && response !== null && 'message' in response
+
+    if (!isObjectWithMessage) {
+      return null
+    }
+
+    const isStringArray = (value: unknown): value is string[] => {
+      return Array.isArray(value) && value.every((m) => typeof m === 'string')
+    }
+
+    if (typeof response.message === 'string' || isStringArray(response.message)) {
+      return response.message
+    }
+
+    return null
   }
 }
 
