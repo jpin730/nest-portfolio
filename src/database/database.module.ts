3d5e00235039b257c86235f14ecbd49f9cb99f4c
Jaime Pineda <jpin730@pm.me>
2026-05-24 14:56:30 -0600

    refactor: :wrench: update database module with dynamic logging

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
+import { ApiConfigModule } from '@api-config/api-config.module'
+import { ApiConfigService } from '@api-config/api-config.service'
+import { Env } from '@api-config/env'
+
 import { DataSourceConfig } from './data-source'
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      ...DataSourceConfig,
-      logging: false,
+    TypeOrmModule.forRootAsync({
+      imports: [ApiConfigModule],
+      useFactory: (apiConfigService: ApiConfigService) => {
+        const env = apiConfigService.env
+        const logging = env !== Env.PRODUCTION
+        return { ...DataSourceConfig, logging }
+      },
+      inject: [ApiConfigService],
     }),
   ],
 })
