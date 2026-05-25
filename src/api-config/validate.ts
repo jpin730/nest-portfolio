964f1f6b2e28a4e6b2016d7930702ba5bc1b9440
Jaime Pineda <jpin730@pm.me>
2026-05-25 14:36:57 -0600

    refactor: :recycle: simplify validation logic by utilizing validateAndParse utility

--- src/api-config/validate.ts
+++ src/api-config/validate.ts
@@ -1,29 +1,14 @@
-import { plainToInstance } from 'class-transformer'
-import { validateSync } from 'class-validator'
+import { validateAndParse } from '@common/utils/validate-and-parse'
+
 import { EnvironmentVariables, MigrationEnvironmentVariables } from './environment-variables'
 
-const validateGeneric = <T extends object>(
-  config: Record<string, unknown>,
-  EnvironmentVariablesClass: new () => T,
-): T => {
-  const validatedConfig = plainToInstance(EnvironmentVariablesClass, config)
-
-  const errors = validateSync(validatedConfig, { skipMissingProperties: false })
-
-  if (errors.length > 0) {
-    throw new Error(errors.toString())
-  }
-
-  return validatedConfig
-}
-
 export const validate = (config: Record<string, unknown>): EnvironmentVariables =>
-  validateGeneric(config, EnvironmentVariables)
+  validateAndParse(EnvironmentVariables, config)
 
 export const validateMigration = (
   config: Record<string, unknown>,
 ): Pick<EnvironmentVariables, 'DB_HOST' | 'DB_PORT' | 'DB_NAME' | 'DB_USER' | 'DB_PASS'> => {
-  const env = validateGeneric(config, MigrationEnvironmentVariables)
+  const env = validateAndParse(MigrationEnvironmentVariables, config)
   return {
     ...env,
     DB_USER: env.DB_MIG_USER,
---
+++ src/common/utils/validate-and-parse.ts
@@ -0,0 +1,17 @@
+import { ClassConstructor, plainToInstance } from 'class-transformer'
+import { validateSync } from 'class-validator'
+
+export const validateAndParse = <T extends object>(
+  classType: ClassConstructor<T>,
+  planObject: unknown,
+): T => {
+  if (typeof planObject !== 'object' || planObject === null) {
+    throw new Error('Invalid plan object')
+  }
+  const instance = plainToInstance(classType, planObject)
+  const errors = validateSync(instance, { skipMissingProperties: false })
+  if (errors.length > 0) {
+    throw new Error(errors.toString())
+  }
+  return instance
+}
