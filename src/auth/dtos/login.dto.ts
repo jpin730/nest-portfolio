7c347b237566cb0b3a6617eea01ff3db0913410a
Jaime Pineda <jpin730@pm.me>
2026-05-28 17:09:10 -0600

    refactor: :safety_vest: update LoginDto validations

--- src/auth/dtos/login.dto.ts
+++ src/auth/dtos/login.dto.ts
@@ -1,5 +1,14 @@
 import { PickType } from '@nestjs/mapped-types'
+import { Transform } from 'class-transformer'
+import { IsString, Length } from 'class-validator'
+
+import { trimTransform } from '@common/transforms/trim.transform'
 
 import { UserDto } from './user.dto'
 
-export class LoginDto extends PickType(UserDto, ['email', 'password']) {}
+export class LoginDto extends PickType(UserDto, ['email']) {
+  @Transform(trimTransform)
+  @IsString()
+  @Length(8, 20)
+  password: string
+}
